85cd2d8d692828f9bc1d26763ac744f4be145f09
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2021-09-05 16:54:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -17,3 +17,22 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   })
+
+
+const textWrap = document.querySelector('.ml9 .letters');
+textWrap.innerHTML = textWrap.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml9 .letter',
+      scale: [0, 1],
+      duration: 1500,
+      elasticity: 600,
+      delay: (el, i) => 45 * (i+1)
+    }).add({
+      targets: '.ml9',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
